bfeaacd024af27cecbbacc932c882a9da93688ae
moni212 <86613894+moni212@users.noreply.github.com>
2021-07-12 12:19:10 +0530

    Assignment 3

---
+++ Assignment_3/index.js
@@ -0,0 +1,6 @@
+function takeValue() {
+    var inp = document.getElementById('inp');
+    var data = inp.value;
+    // console.log(inp.value);
+    document.getElementById('name').innerText = data;
+}
